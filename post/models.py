24be08de0ae7dbfca1495543b6bd3e16f8d4263b
Kenmogne <kenmognethimotee@gmail.com>
2021-05-20 16:49:16 +0100

    firstCommit

---
+++ post/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from core.models import CustumUser, StyleImage
+
+# Create your models here.
+
+
+class Image(models.Model):
+
+    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return "Image -- "+str(self.post)
+
+class Post(models.Model):
+
+    texte = models.TextField()
+    date_publication = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustumUser, on_delete=models.CASCADE)
+    styleImage = models.ForeignKey(StyleImage, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "texte"
+    pass 
+
+class ClickForStyle(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    click_date = models.DateTimeField(auto_now_add=True)
+
+
+class PostComment(models.Model):
+
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustumUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    response = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contenu
+    pass 
+
+class UpVote(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    vote_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustumUser, on_delete=models.CASCADE)
+
+    pass 
+
+class DownVote(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    vote_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustumUser, on_delete=models.CASCADE)
+
+    pass
+private = models.BooleanField(default=False)
+class LikeComment(models.Model):
+
+    comment = models.ForeignKey(PostComment, on_delete=models.CASCADE)
+    vote_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustumUser, on_delete=models.CASCADE)
+    pass 
+
+class DislikeComment(models.Model):
+
+    comment = models.ForeignKey(PostComment, on_delete=models.CASCADE)
+    vote_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustumUser, on_delete=models.CASCADE)
+    pass 
+
+
